a0346146f0cba333822cc2c273ca4a12e87fbc14
heaven9099 <hasegawaheaven@gmail.com>
2025-05-17 12:40:44 +0000

    onchain mod

--- onchain/programs/tmp/src/swaps/mod.rs
+++ onchain/programs/tmp/src/swaps/mod.rs
@@ -1,11 +1,18 @@
 // File: program/src/swaps/mod.rs
 
-pub mod jupiter;
 pub mod meteora;
 pub mod orca;
-pub mod raydium;
+pub mod meteora_dlmm;
+pub mod meteora_pools;
+pub mod raydium_clmm;
+pub mod raydium_cpmm;
+pub mod raydium_amm;
 
-pub use jupiter::*;
+
 pub use meteora::*;
 pub use orca::*;
-pub use raydium::*;
+pub use raydium_clmm::*; 
+pub use raydium_cpmm::*; 
+pub use raydium_amm::*;
+pub use meteora_dlmm::*;
+pub use meteora_pools::*;
