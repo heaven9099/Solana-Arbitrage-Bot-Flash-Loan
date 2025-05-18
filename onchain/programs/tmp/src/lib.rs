0958893846e1b217d8bd8c405b1264a57999b14c
heaven9099 <hasegawaheaven@gmail.com>
2025-05-18 16:20:29 +0000

    update lin , will update

--- onchain/programs/tmp/src/lib.rs
+++ onchain/programs/tmp/src/lib.rs
@@ -8,25 +8,16 @@ pub mod state;
 pub mod swaps;
 pub mod error;
 
-use swaps::orca::*;
-use swaps::raydium::*;
-use swaps::meteora::*;
-use swaps::jupiter::*;
+use swaps::*;
+use swaps::me                                                                   use swaps::meteora::*;
+
+
 
 declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 
 #[program]
 pub mod tmp {
     use super::*;
-
-    pub fn initialize_raydium_swap_state(ctx: Context<InitializeRaydiumSwapState>) -> Result<()> {
-        let swap_state = &mut ctx.accounts.swap_state;
-        swap_state.bump = *ctx.bumps.get("swap_state").unwrap();
-        swap_state.authority = ctx.accounts.authority.key();
-        swap_state.initialized = true;
-        swap_state.last_swap_timestamp = Clock::get()?.unix_timestamp;
-        Ok(())
-    }
 
     pub fn orca_swap(
         ctx: Context<OrcaSwap>,
@@ -52,29 +43,6 @@ pub mod tmp {
         ctx.accounts.process_swap(amount_in, minimum_amount_out)
     }
 
-    pub fn jupiter_swap(
-        ctx: Context<JupiterSwap>,
-        amount_in: u64,
-        minimum_amount_out: u64,
-        route_data: Vec<u8>,
-    ) -> Result<()> {
-        ctx.accounts.process_swap(amount_in, minimum_amount_out, route_data)
-    }
+
 }
 
-#[derive(Accounts)]
-pub struct InitializeRaydiumSwapState<'info> {
-    #[account(
-        init,
-        payer = authority,
-        space = RaydiumSwapState::LEN,
-        seeds = [b"raydium_swap_state", authority.key().as_ref()],
-        bump
-    )]
-    pub swap_state: Account<'info, RaydiumSwapState>,
-    
-    #[account(mut)]
-    pub authority: Signer<'info>,
-    
-    pub system_program: Program<'info, System>,
-}
